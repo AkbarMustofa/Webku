1ebed9d0afacf210ddd0bb9873bbe60d0f42965d
AkbarMustofa <109795534+AkbarMustofa@users.noreply.github.com>
2023-08-25 10:58:04 +0700

    Add files via upload

---
+++ js.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded"), function() {
+    const downloadButton = document.getEventListener("clicl", function() {
+
+        const link = document.createElement("a");
+        link.href = cvPath;
+    })
+
+
+
+
+}
